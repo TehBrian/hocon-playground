50dc28dda6a21c5aee3972453bbac045cc4c35ae
TehBrian <tehbrian@proton.me>
2024-08-19 13:12:55 -0400

    minor syntax cleanup

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,10 +1,9 @@
 package controllers
 
-import javax.inject._
-import play.api._
-import play.api.mvc._
 import com.typesafe.config._
+import play.api.mvc._
 
+import javax.inject._
 import scala.util.{Failure, Success, Try}
 
 @Singleton
@@ -17,11 +16,11 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
   val confResolverOptions = ConfigResolveOptions.noSystem()
 
   def hoconToJson() = Action(parse.tolerantText) { implicit request =>
-    val resolve = request.queryString.getOrElse("resolve", Seq()).headOption.map(_.toBoolean).getOrElse(false);
-    val json = request.queryString.getOrElse("json", Seq()).headOption.map(_.toBoolean).getOrElse(false);
-    val formatted = request.queryString.getOrElse("formatted", Seq()).headOption.map(_.toBoolean).getOrElse(false);
-    val comments = request.queryString.getOrElse("comments", Seq()).headOption.map(_.toBoolean).getOrElse(false);
-    val originComments = request.queryString.getOrElse("originComments", Seq()).headOption.map(_.toBoolean).getOrElse(false);
+    val resolve = request.queryString.getOrElse("resolve", Seq()).headOption.exists(_.toBoolean);
+    val json = request.queryString.getOrElse("json", Seq()).headOption.exists(_.toBoolean);
+    val formatted = request.queryString.getOrElse("formatted", Seq()).headOption.exists(_.toBoolean);
+    val comments = request.queryString.getOrElse("comments", Seq()).headOption.exists(_.toBoolean);
+    val originComments = request.queryString.getOrElse("originComments", Seq()).headOption.exists(_.toBoolean);
     val parseOptions = ConfigParseOptions.defaults().setIncluder(new ProhibitIncluder());
     val conf = Try(ConfigFactory.parseString(request.body, parseOptions))
 
