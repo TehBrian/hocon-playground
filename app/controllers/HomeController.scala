84859133b60495fdaf74a05dfcd856a79dadf19d
TehBrian <tehbrian@proton.me>
2024-08-19 13:37:09 -0400

    update source URL; use labels for inputs; misc cleanup

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -8,24 +8,23 @@ import scala.util.{Failure, Success, Try}
 
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-
-  def index() = Action { implicit request: Request[AnyContent] =>
+  def index(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
 
-  val confResolverOptions = ConfigResolveOptions.noSystem()
+  private val confResolverOptions = ConfigResolveOptions.noSystem()
 
-  def hoconToJson() = Action(parse.tolerantText) { implicit request =>
-    val resolve = request.queryString.getOrElse("resolve", Seq()).headOption.exists(_.toBoolean);
-    val json = request.queryString.getOrElse("json", Seq()).headOption.exists(_.toBoolean);
-    val formatted = request.queryString.getOrElse("formatted", Seq()).headOption.exists(_.toBoolean);
-    val comments = request.queryString.getOrElse("comments", Seq()).headOption.exists(_.toBoolean);
-    val originComments = request.queryString.getOrElse("originComments", Seq()).headOption.exists(_.toBoolean);
-    val parseOptions = ConfigParseOptions.defaults().setIncluder(new ProhibitIncluder());
+  def hoconToJson(): Action[String] = Action(parse.tolerantText) { implicit request =>
+    val resolve = request.queryString.getOrElse("resolve", Seq()).headOption.exists(_.toBoolean)
+    val json = request.queryString.getOrElse("json", Seq()).headOption.exists(_.toBoolean)
+    val formatted = request.queryString.getOrElse("formatted", Seq()).headOption.exists(_.toBoolean)
+    val comments = request.queryString.getOrElse("comments", Seq()).headOption.exists(_.toBoolean)
+    val originComments = request.queryString.getOrElse("originComments", Seq()).headOption.exists(_.toBoolean)
+    val parseOptions = ConfigParseOptions.defaults().setIncluder(new ProhibitIncluder())
     val conf = Try(ConfigFactory.parseString(request.body, parseOptions))
 
     conf match {
-      case Success(conf) => {
+      case Success(conf) =>
         val resolvedConf = if (resolve) conf.resolve(confResolverOptions) else conf
         val renderConfig = ConfigRenderOptions.defaults()
           .setJson(json)
@@ -34,7 +33,6 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
           .setOriginComments(originComments);
         val rendered = resolvedConf.root().render(renderConfig)
         Ok(rendered)
-      }
       case Failure(err) => Ok(err.getMessage)
       case _ => Ok("internal error")
     }
