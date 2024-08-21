756dad92cf61ab418cb0142e113b5aa3629a21d9
TehBrian <tehbrian@proton.me>
2024-08-20 21:56:29 -0400

    replace querySelector with getElementById

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,18 +1,19 @@
-let hocon = window.document.querySelector('#hocon');
-let output = window.document.querySelector('#output');
+let hocon = window.document.getElementById('hocon');
+let output = window.document.getElementById('output');
 
-let inputResolve = window.document.querySelector('#resolve');
-let inputJson = window.document.querySelector('#json');
-let inputFormatted = window.document.querySelector('#formatted');
-let inputComments = window.document.querySelector('#comments');
-let inputOriginComments = window.document.querySelector('#originComments');
+let inputResolve = window.document.getElementById('resolve');
+let inputJson = window.document.getElementById('json');
+let inputFormatted = window.document.getElementById('formatted');
+let inputComments = window.document.getElementById('comments');
+let inputOriginComments = window.document.getElementById('originComments');
+
+hocon.addEventListener('keyup', updateOutput);
 
 inputResolve.addEventListener('input', updateOutput)
 inputJson.addEventListener('input', updateOutput)
 inputFormatted.addEventListener('input', updateOutput)
 inputComments.addEventListener('input', updateOutput)
 inputOriginComments.addEventListener('input', updateOutput)
-hocon.addEventListener('keyup', updateOutput);
 
 function updateOutput() {
     const query =
